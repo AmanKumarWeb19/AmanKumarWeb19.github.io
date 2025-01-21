dd29dc8547218db528b1aa150f62f169feb044b1
Aman Kumar <akgupta0524@gmail.com>
2025-01-21 17:13:12 +0530

    update link icon

--- src/Components/Skills/skill.js
+++ src/Components/Skills/skill.js
@@ -1,6 +1,7 @@
 export const data = [
   { img: "./html.svg", title: "HTML" },
   { img: "./css.svg", title: "CSS" },
+  { img: "./tailwind-css.svg", title: "Tailwind-Css" },
   { img: "./javascript.svg", title: "JS" },
   { img: "./react.svg", title: "React" },
   { img: "./redux.svg", title: "Redux" },
@@ -8,4 +9,5 @@ export const data = [
   { img: "./expressjs.svg", title: "Express" },
   { img: "./mongodb.svg", title: "MongoDB" },
   { img: "./postman.svg", title: "Postman" },
+  { img: "./prompt.png", title: "Prompt Engineering" },
 ];
